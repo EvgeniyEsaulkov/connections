a2fee0b92a27cc2722831791e42ceba42c5f397e
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-06-07 15:56:32 +0300

    apply foundation classes

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Popper from "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import "foundation-sites/dist/css/foundation";
 import App from "../components/App";
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <App />,
-    document.body.appendChild(document.createElement("div")),
+    document.body.appendChild(document.createElement("main")),
   );
 });
