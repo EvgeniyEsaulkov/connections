a296e168aad8ab1d27bb3154c3d3344201aecf55
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-07-26 11:31:51 +0300

    add event type to graphql

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,7 +2,7 @@ class GraphqlController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
 
   def execute
     variables = ensure_hash(params[:variables])
@@ -12,7 +12,7 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = ConnectionsSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = ApplicationSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue => e
     raise e unless Rails.env.development?
