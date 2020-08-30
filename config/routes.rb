3b6b70d9169b5ebe9110bbdf953ba08e97d83516
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-08-30 18:34:20 +0300

    remove api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,5 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   root "home#index"
 
-  namespace :api do
-    namespace :v1 do
-      get "events/index"
-      post "events/create"
-      get "/show/:id", to: "events#show"
-      delete "/destroy/:id", to: "events#destroy"
-    end
-  end
-
   get "/*path", to: "home#index"
 end
