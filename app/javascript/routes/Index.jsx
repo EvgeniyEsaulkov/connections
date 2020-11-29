60cbd9f9911868e9b8d71feb1304da21968d2f90
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-11-29 16:47:31 +0300

    remove route from sign in form

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Events from "../components/Events";
 import Event from "../components/Event";
-import Login from "../forms/login";
 
 export default (
   <Router>
@@ -11,7 +10,6 @@ export default (
       <Route path="/" exact component={Home} />
       <Route path="/events" exact component={Events} />
       <Route path="/event/:id" exact component={Event} />
-      <Route path="/sign_in" exact component={Login} />
     </Switch>
   </Router>
 );
